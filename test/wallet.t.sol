743099f0a805ccdb1b2424eea7ab97177a064b47
ethface <pwvux@tuta.io>
2026-03-02 22:08:35 +0000

    audit check

--- test/wallet.t.sol
+++ test/wallet.t.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {Wallet} from "../src/Wallet.sol";
 
 contract WalletTest is Test {
@@ -20,5 +20,22 @@ contract WalletTest is Test {
         vm.expectRevert();
         wallet.setOwner(newOwner);
         vm.stopPrank();
+    }
+
+    function testEthBalance() public {
+        console.log("Initial balance:", address(this).balance / 1e18);
+    }
+
+    function _send(uint256 _amount) internal {
+        (bool success, ) = address(wallet).call{value: _amount}("");
+        require(success, "Failed to send Ether");
+    }
+
+    function testSendEth() public {
+        hoax(address(1), 10 ether); // deal + prank in one step
+        _send(1 ether);
+        console.log("Balance after sending:", address(wallet).balance / 1e18);
+        assertEq(address(wallet).balance, 1 ether);
+        assertEq(address(1).balance, 9 ether);
     }
 }
